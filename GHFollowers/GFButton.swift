7d055d6957ea8e5dd8fa6c1f1ea502bec3568e60
pdimarcodev <pablo.d.dimarco@gmail.com>
2022-04-30 19:21:27 -0300

    feat: custom button and search field added

--- GHFollowers/GFButton.swift
+++ GHFollowers/GFButton.swift
@@ -26,9 +26,9 @@ class GFButton: UIButton {
     }
     
     private func configure() {
-        layer.cornerRadius      = 10
-        titleLabel?.textColor   = .white
-        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
