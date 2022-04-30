bf14a86007eec0d47a0cc6135d5174c0358feb25
pdimarcodev <pablo.d.dimarco@gmail.com>
2022-04-30 13:17:04 -0300

    chore: project setup

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Pablo Dario Di Marco on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
