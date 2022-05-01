6dab8c908c39fe3503b8ae68bc402c3a0256ba4f
pdimarcodev <pablo.d.dimarco@gmail.com>
2022-04-30 23:32:51 -0300

    feat: custom alert

---
+++ GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,32 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Pablo Dario Di Marco on 4/30/22.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
