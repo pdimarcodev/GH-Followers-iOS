be4a3723a8bc712ac2db74181a3331c0baa73e61
pdimarcodev <pablo.d.dimarco@gmail.com>
2022-05-01 19:17:06 -0300

    chore: models added

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Pablo Dario Di Marco on 5/1/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
